620eedb68b09f094c5638aaf6d2c4704d0c16e9f
Akash Chavda <36260957+skychavda@users.noreply.github.com>
2019-03-20 13:02:17 +0530

    Add files via upload

---
+++ precache-manifest.3c20a15aa3ec10c9e3576f5d3eb808cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "abf0bcfadd034267a525",
+    "url": "./static/js/main.f1037fb2.chunk.js"
+  },
+  {
+    "revision": "5f056c885d78ff71cf42",
+    "url": "./static/js/2.d28df4e3.chunk.js"
+  },
+  {
+    "revision": "abf0bcfadd034267a525",
+    "url": "./static/css/main.144b2a65.chunk.css"
+  },
+  {
+    "revision": "7068cd0fe43090709b45ee3f23ce6c98",
+    "url": "./index.html"
+  }
+];
